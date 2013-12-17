06935ac58da0e8d5976dbcf68e668c800e58d5d1
Shasheen Ediriweera <shasheene@gmail.com>
2013-12-17 22:17:03 +1030

    Logic of each indivual piece completed but bugs may exist. No castling yet. Adding GUI is next milestone

--- chess.py
+++ chess.py
@@ -12,11 +12,14 @@ class Piece(object):
 		self.moved = 0
 		self.col = col
 		self.type="_"
+		self.c=""
 		if (self.col=="white"):
 			self.enemyCol="black"
+			self.c="w"#printing only
 			self.forwardDir=-1
 		elif (self.col=="black"):
 			self.enemyCol="white"
+			self.c="b"
 			self.forwardDir=+1
 
 	def getMoveSet(self,pieceLocation):
@@ -28,20 +31,84 @@ class Pawn(Piece):
 		self.type="p"
 
 	def getMoveSet(self,pieceLocation):
-		myMoveSet=[]
-		if (checkLocation(pieceLocation[0]+self.forwardDir,pieceLocation[1])=="_"): #Is space directly forward from us free?
-			myMoveSet.append([pieceLocation[0]+self.forwardDir,pieceLocation[1]])
-			if ((checkLocation(pieceLocation[0]+self.forwardDir*2,pieceLocation[1])=="_") and self.moved==0): #If in addition space TWO forward from us free, and we haven't moved
-				myMoveSet.append([pieceLocation[0]+self.forwardDir*2,pieceLocation[1]]) #Pawn jump
-		if (checkLocation(pieceLocation[0]+self.forwardDir,pieceLocation[1]-1)==self.enemyCol): #Attack north west
-			myMoveSet.append([pieceLocation[0]+self.forwardDir,pieceLocation[1]-1])
-		if (checkLocation(pieceLocation[0]+self.forwardDir,pieceLocation[1]+1)==self.enemyCol): #Attack north east
-			myMoveSet.append([pieceLocation[0]+self.forwardDir,pieceLocation[1]+1])
-			
-		return myMoveSet
+		self.myMoveSet=[]
+		if (pieceAt(pieceLocation[0]+self.forwardDir,pieceLocation[1]).type=="_"): #Is space directly forward from us free?
+			self.myMoveSet.append([pieceLocation[0]+self.forwardDir,pieceLocation[1]])
+			if ((pieceAt(pieceLocation[0]+self.forwardDir*2,pieceLocation[1]).type=="_") and self.moved==0): #If in addition space TWO forward from us free, and we haven't moved
+				self.myMoveSet.append([pieceLocation[0]+self.forwardDir*2,pieceLocation[1]]) #Pawn jump
+		if (pieceAt(pieceLocation[0]+self.forwardDir,pieceLocation[1]-1).col==self.enemyCol): #Attack north west
+			self.myMoveSet.append([pieceLocation[0]+self.forwardDir,pieceLocation[1]-1])
+		if (pieceAt(pieceLocation[0]+self.forwardDir,pieceLocation[1]+1).col==self.enemyCol): #Attack north east
+			self.myMoveSet.append([pieceLocation[0]+self.forwardDir,pieceLocation[1]+1])
+		self.moved=1
+		return self.myMoveSet
 
-def checkLocation(row,column):
-	return board[row][column].type
+
+class AdvancedPiece(Piece):
+	def __init__(self,col,listOfUnitMoves,movementStyle):
+		super(AdvancedPiece, self).__init__(col)
+		self.myVectorSet = listOfUnitMoves
+ #movementStyle either "slider" (ie rook,bishop,queen) or "teleporter" (king,knight) with respect to moveVectors
+		self.movementStyle= movementStyle
+	def getMoveSet(self,pieceLocation):
+		self.myMoveSet=[]
+		for vector in self.myVectorSet:
+			self.i = pieceLocation[0]+vector[0]
+			self.j = pieceLocation[1]+vector[1]
+			#print 'Checking vector',
+			#print vector
+			if self.movementStyle=="slider":
+				while (isOffEdge(self.i,self.j)==0 and pieceAt(self.i,self.j).col!=self.col):
+					#print 'Adding: ' + str(self.i) + "," + str(self.j)
+					self.myMoveSet.append([self.i,self.j])
+					self.i += vector[0]
+					self.j += vector[1]
+			elif self.movementStyle=="teleporter":
+				if (isOffEdge(self.i,self.j)==0 and pieceAt(self.i,self.j).col!=self.col):
+					#print 'Adding: ' + str(self.i) + "," + str(self.j)
+					self.myMoveSet.append([self.i,self.j])
+
+		self.moved=1
+		return self.myMoveSet
+
+class Rook(AdvancedPiece):
+	def __init__(self,col):
+		self.myVectorSet = [[-1,0],[1,0],[0,-1],[0,1]]
+		super(Rook, self).__init__(col,self.myVectorSet,"slider")
+		self.type="r"
+
+class Bishop(AdvancedPiece):
+	def __init__(self,col):
+		self.myVectorSet = [[-1,-1],[-1,+1],[+1,-1],[1,1]]
+		super(Bishop, self).__init__(col,self.myVectorSet,"slider")
+		self.type="b"
+
+class Queen(AdvancedPiece):
+	def __init__(self,col): #Both rook AND bishop's movesets
+		self.myVectorSet = [[-1,0],[1,0],[0,-1],[0,1],[-1,-1],[-1,+1],[+1,-1],[1,1]]
+		super(Queen, self).__init__(col,self.myVectorSet,"slider")
+		self.type="q"
+
+class King(AdvancedPiece):
+	def __init__(self,col): #Both rook AND bishop's movesets
+		self.myVectorSet = [[-1,0],[1,0],[0,-1],[0,1],[-1,-1],[-1,+1],[+1,-1],[1,1]]
+		super(King, self).__init__(col,self.myVectorSet,"teleporter")
+		self.type="k"
+		#No castling yet
+
+class Knight(AdvancedPiece):
+	def __init__(self,col): #Both rook AND bishop's movesets
+		self.myVectorSet = [[-2,-1],[-2,1],[1,2],[-1,2],[2,-1],[2,1],[-1,-2],[1,-2]]
+		super(Knight, self).__init__(col,self.myVectorSet,"teleporter")
+		self.type="h" #h for 'horse', as king is taken
+
+def pieceAt(row,column): #Conveniant notation
+	return board[row][column]
+
+def isOffEdge(i,j): # Remember internal board is indexed 0 to 7, not 1 to 8
+	if i>=7 or i<0 or j>=7 or j<0:
+		return 1
+	return 0
 
 def convert(pair):
 	""" Converts chess coords to index (eg. 'a4' to [4,0] or 'e3' to [3,5]). Returns in [row,col], Assumes valid input (for now) """
@@ -60,7 +127,10 @@ def take_input():
 def printBoard(board):
 	for row in board[:]:
 		for piece in row[:]:
-			print piece.type + " ",
+			if piece.type!="_":
+				print piece.type + "(" + piece.c + ") ",
+			else:
+				print '____ ', # just for prettyness
 		print #next row
 	print #new line
 
@@ -68,59 +138,66 @@ def printBoard(board):
 global board
 board = []
 
+#Board creation:
 piece = Piece("black") #test only
-board.append([piece,piece,piece,piece,piece,piece,piece,piece])
+r = Rook("black")
+h = Knight("black")
+b = Bishop("black")
+q = Queen("black")
+k = King("black")
+board.append([r,h,b,q,k,b,h,r])
 board.append([]);
 for i in range(0,8):
 	p = Pawn("black")
 	board[1].append(p);
-piece = Piece("") #test only
+blankPiece = Piece("") #test only
 for i in range(2,6):
-	board.append([piece,piece,piece,piece,piece,piece,piece,piece])
+	board.append([blankPiece,blankPiece,blankPiece,blankPiece,blankPiece,blankPiece,blankPiece,blankPiece])
 board.append([]);
 for i in range(0,8):
 	p = Pawn("white")
 	board[6].append(p);
 piece = Piece("white") #test only
-board.append([piece,piece,piece,piece,piece,piece,piece,piece])
+r = Rook("white")
+h = Knight("white")
+b = Bishop("white")
+q = Queen("white")
+k = King("white")
+board.append([r,h,b,k,q,b,h,r])
 
 
-"""board = [
-					["r","h","b","q","k","b","h","r"],
-					["p","p","p","p","p","p","p","p"],
-					["","","","","","","",""],
-					["","","","","","","",""],
-					["","","","","","","",""],
-					["","","","","","","",""],
-					["p","p","p","p","p","p","p","p"],
-					["r","h","b","k","q","b","h","r"]
-				]"""
 
 print 'WELCOME TO TEXT BASED CHESS'
 printBoard(board)
 
 while 1:
-	print 'White\'s turn. Enter row'
-	legalSelection=0
-	while (legalSelection==0):
+	print 'White\'s turn. Select piece'
+	
+	moveSetSize=0
+	while (moveSetSize==0):
 		selected = take_input()
 		print selected
-		legalSelection=1 #No error checking for now
-		if (legalSelection==0):
-			print 'INVALID MOVE. Re-enter'
 
-	selected = convert(selected[0])
-	printBoard(board)
-	selectedMoveSet=board[selected[0]][selected[1]].getMoveSet(selected)
-	print selectedMoveSet
-	print 'Please enter index of move choice to make:'
-	indexOfChoice = int(raw_input())
-	end = selectedMoveSet[indexOfChoice]
-	
-	print 'Chose:',
-	print end
+		selected = convert(selected[0])
+		printBoard(board)
+		selectedMoveSet=board[selected[0]][selected[1]].getMoveSet(selected)
+		print selectedMoveSet
+		moveSetSize=len(selectedMoveSet)
+		if moveSetSize==0:
+			print 'Error no moves available. Choose another piece'
 
+	legalMoveChoice=0
+	while(legalMoveChoice==0):
+		print 'Please enter index of move choice to make:'
+		indexOfChoice = int(raw_input())
+		end = selectedMoveSet[indexOfChoice]
+		if (indexOfChoice>=0 and indexOfChoice<len(selectedMoveSet)):
+			print 'Chose:',
+			print end
+			legalMoveChoice=1
+		else:
+			print 'Illegal move choice: ' + str(indexOfChoice)
 
 	board[end[0]][end[1]] = board[selected[0]][selected[1]]
-	board[selected[0]][selected[1]] = piece
+	board[selected[0]][selected[1]] = blankPiece
 	printBoard(board)
