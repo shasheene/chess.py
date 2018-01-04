266961cfe005066a004d807d61676bcc02bc1014
Shasheen Ediriweera <shasheene@gmail.com>
2018-01-04 14:04:00 +1030

    Fixes Rook/Bishop edge of board bug

--- chess.py
+++ chess.py
@@ -160,8 +160,9 @@ def pieceAtCoords(coords):  # Conveniant notation
     return board[coords[0]][coords[1]]
 
 
-def isOffEdge(i, j):  # Remember internal board is indexed 0 to 7, not 1 to 8
-    if i >= 7 or i < 0 or j >= 7 or j < 0:
+# Recall, internal board (unlike raw user input) is indexed 0 to 7, not 1 to 8.
+def isOffEdge(i, j):
+    if i > 7 or i < 0 or j > 7 or j < 0:
         return 1
     return 0
 
